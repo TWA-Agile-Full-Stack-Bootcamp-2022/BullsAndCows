6fdd3974407406a219af432d0f056227a95a1272
Guangguang Ma <ggma@thoughtworks.com>
2022-10-11 23:53:27 +0800

    feat: check guess number

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BullsAndCows
 {
     public class BullsAndCowsGame
     {
         private readonly SecretGenerator secretGenerator;
+
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
@@ -14,7 +17,33 @@ namespace BullsAndCows
 
         public string Guess(string guess)
         {
+            if (!CheckGuess(guess))
+            {
+                return "Wrong Input, input again";
+            }
+
             throw new NotImplementedException();
+        }
+
+        public bool CheckGuess(string guess)
+        {
+            if (guess.Length != 4)
+            {
+                return false;
+            }
+
+            if (!guess.All(char.IsDigit))
+            {
+                return false;
+            }
+
+            HashSet<char> set = new HashSet<char>(guess.ToCharArray());
+            if (set.Count != 4)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -13,5 +13,15 @@ namespace BullsAndCowsTest
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
         }
+
+        [Fact]
+        public void Should_return_error_message_when_input_guess_number_is_wrong()
+        {
+            var secretGenerator = new SecretGenerator();
+            var game = new BullsAndCowsGame(secretGenerator);
+            Assert.Equal("Wrong Input, input again", game.Guess("1A23"));
+            Assert.Equal("Wrong Input, input again", game.Guess("1123"));
+            Assert.Equal("Wrong Input, input again", game.Guess("12345"));
+        }
     }
 }
