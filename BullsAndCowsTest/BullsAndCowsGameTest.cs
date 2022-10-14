995fcb76f5008e84bd503b08d374bf59da7f4cce
Guangguang Ma <ggma@thoughtworks.com>
2022-10-14 17:50:41 +0800

    refactor: simplify test using parameter

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -15,29 +15,32 @@ namespace BullsAndCowsTest
             Assert.True(game.CanContinue);
         }
 
-        [Fact]
-        public void Should_return_error_message_when_input_guess_number_is_wrong()
+        [Theory]
+        [InlineData("12")]
+        [InlineData("1A23")]
+        [InlineData("1123")]
+        [InlineData("12345")]
+        public void Should_return_error_message_when_input_guess_number_is_wrong(string guess)
         {
             var secretGenerator = new SecretGenerator();
             var game = new BullsAndCowsGame(secretGenerator);
-            Assert.Equal("Wrong Input, input again", game.Guess("1A23"));
-            Assert.Equal("Wrong Input, input again", game.Guess("1123"));
-            Assert.Equal("Wrong Input, input again", game.Guess("12345"));
+            Assert.Equal("Wrong Input, input again", game.Guess(guess));
         }
 
-        [Fact]
-        public void Should_return_guess_result_when_input_guess_number()
+        [Theory]
+        [InlineData("1567", "1A0B")]
+        [InlineData("2478", "0A2B")]
+        [InlineData("0324", "1A2B")]
+        [InlineData("5678", "0A0B")]
+        [InlineData("4321", "0A4B")]
+        [InlineData("1234", "4A0B")]
+        public void Should_return_guess_result_when_input_guess_number(string guess, string result)
         {
             var secretGenerator = new Mock<SecretGenerator>();
             secretGenerator.Setup(s => s.GenerateSecret()).Returns("1234");
             var game = new BullsAndCowsGame(secretGenerator.Object);
 
-            Assert.Equal("1A0B", game.Guess("1567"));
-            Assert.Equal("0A2B", game.Guess("2478"));
-            Assert.Equal("1A2B", game.Guess("0324"));
-            Assert.Equal("0A0B", game.Guess("5678"));
-            Assert.Equal("0A4B", game.Guess("4321"));
-            Assert.Equal("4A0B", game.Guess("1234"));
+            Assert.Equal(result, game.Guess(guess));
         }
     }
 }
