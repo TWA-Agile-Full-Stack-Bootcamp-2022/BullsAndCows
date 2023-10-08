7273fb25491eabd603800e515cfc4ecd94fad160
Hong Wu <hong.wu@rea-group.com>
2023-10-08 15:45:34 +0800

    test: should return error message when number of guesses exceeds 6

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -13,5 +13,23 @@ namespace BullsAndCowsTest
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
         }
+        [Theory]
+        [InlineData("1234")]
+        public void Should_return_error_message_when_number_of_guesses_exceeds_6(string secret)
+        {
+            // given
+            var secretGenerator = new Mock<SecretGenerator>();
+            secretGenerator.Setup(s => s.GenerateSecret()).Returns(secret);
+            var game = new BullsAndCowsGame(secretGenerator.Object);
+
+            // when
+            for (var count = 0; count < 6; count++)
+            {
+                game.Guess("5678");
+            }
+
+            // then
+            Assert.Equal("Run out of chances", game.Guess("1234"));
+        }
     }
 }
