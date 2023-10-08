c3436c28cb008bdb4ca7a07cf161303a741cd9bd
Hong Wu <hong.wu@rea-group.com>
2023-10-08 15:49:32 +0800

    feat: implement the bulls and cows string generation

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -31,9 +31,44 @@ namespace BullsAndCows
                 return "Wrong Input, input again";
             }
 
+            string bullsAndCows = CountBullsAndCows(guess);
+
+            // TBC: Whether it is necessary to accumulate numberOfGuesses when the guess is correct
+            // if (bullsAndCows != $"{secret.Length}A{0}B")
+            // {
+            numberOfGuesses++;
+            // }
+
+            return bullsAndCows;
+        }
+
         private bool IsValidGuess(string guess) => guess.Length == secret.Length
             && guess.All(char.IsDigit)
             && new HashSet<char>(guess.ToCharArray()).Count == secret.Length;
+
+        private string CountBullsAndCows(string guess)
+        {
+            int bulls = 0;
+            int cows = 0;
+
+            char[] guessArr = guess.ToCharArray();
+            char[] secretArr = secret.ToCharArray();
+
+            for (int i = 0; i < guessArr.Length; i++)
+            {
+                if (secretArr[i] == guessArr[i])
+                {
+                    bulls++;
+                    continue;
+                }
+
+                if (Array.IndexOf(secretArr, guessArr[i]) >= 0)
+                {
+                    cows++;
+                }
+            }
+
+            return $"{bulls}A{cows}B";
         }
     }
 }
