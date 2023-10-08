14538716f4eea6e288bb33c2fa7dcab1211b3863
Hong Wu <hong.wu@rea-group.com>
2023-10-08 15:49:03 +0800

    test: should return bulls and cows when guess is valid

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -32,6 +32,28 @@ namespace BullsAndCowsTest
             // then
             Assert.Equal("Wrong Input, input again", output);
         }
+
+        [Theory]
+        [InlineData("1234", "1567", "1A0B")]
+        [InlineData("1234", "2478", "0A2B")]
+        [InlineData("1234", "0324", "1A2B")]
+        [InlineData("1234", "5678", "0A0B")]
+        [InlineData("1234", "4321", "0A4B")]
+        [InlineData("1234", "1234", "4A0B")]
+        public void Should_return_bulls_and_cows_when_guess_is_valid(string secret, string guess, string expectedOutput)
+        {
+            // given
+            var secretGenerator = new Mock<SecretGenerator>();
+            secretGenerator.Setup(s => s.GenerateSecret()).Returns(secret);
+            var game = new BullsAndCowsGame(secretGenerator.Object);
+
+            // when
+            var output = game.Guess(guess);
+
+            // then
+            Assert.Equal(expectedOutput, game.Guess(guess));
+        }
+
         [Theory]
         [InlineData("1234")]
         public void Should_return_error_message_when_number_of_guesses_exceeds_6(string secret)
